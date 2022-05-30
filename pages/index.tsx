4f5d7672b0362ee0c80dc3a49d4c33472459f480
dianlobb <dkaritoloboab@gmail.com>
2022-05-30 12:58:33 -0500

    deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Widgets from "../components/Widgets";
 import Feed from "../components/Feed";
 import { fetchTweets } from '../utils/fetchTweets';
 import { Tweet } from "../typingd"
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import { Toaster } from 'react-hot-toast';
 
 interface Props {
@@ -33,7 +33,7 @@ const Home: NextPage<Props>  = ({tweets}:Props) => {
 
 export default Home
 
-export  const getServerSideProps: GetServerSideProps =async () => {
+export  const getServerSideProps: GetStaticProps =async () => {
     const  tweets = await fetchTweets()
   return{
     props:{
